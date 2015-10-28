965bc4bd39b93dbb4ec26157c1980b9f72d9e587
Kevin Murphy <murphyk3@wit.edu>
2015-10-28 11:36:01 -0400

    Version 1.0.0

---
+++ octoprint_firmwareupdate/__init__.py
@@ -0,0 +1,145 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+__author__ = "Kevin Murphy <kevin@voxel8.co>"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2015 Kevin Murphy - Released under terms of the AGPLv3 License"
+
+import octoprint.plugin
+from octoprint.util import RepeatedTimer
+import flask
+import os
+from subprocess import Popen
+import signal
+import linecache
+import psutil
+import time
+
+class FirmwareUpdatePlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.AssetPlugin,
+                       octoprint.plugin.SettingsPlugin,
+                       octoprint.plugin.SimpleApiPlugin):
+
+    def __init__(self):
+        self.isUpdating = False
+	    self._checkTimer = None
+        self.updatePID = None
+
+    def get_assets(self):
+        return {
+            "js": ["js/firmwareupdate.js"]
+        }
+
+    def startTimer(self, interval):
+        self._checkTimer = RepeatedTimer(interval, self.checkStatus, run_first=True, condition=self.checkStatus)
+        self._checkTimer.start()
+
+    def checkStatus(self):
+	    update_result = open('/home/pi/build_log.txt').read()
+    	if 'No device matching following was found' in update_result:
+    	    self._logger.info("Failed update...")
+            self.isUpdating = False
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="failed", reason="A connected device was not found."))
+    	    return False
+    	elif 'FAILED' in update_result:
+    	    self._logger.info("Failed update...")
+            self.isUpdating = False
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="failed"))
+    	    return False
+    	elif 'bytes of flash verified' in update_result and 'successfully' in update_result :
+            self._logger.info("Successful update!")
+    	    self.isUpdating = False
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="completed"))
+    	    return False
+    	elif 'ReceiveMessage(): timeout' in update_result:
+    	    self._logger.info("Update timed out. Check if port is already in use!")
+    	    self.isUpdating = False
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="failed", reason="Device timed out. Please check that the port is not in use!"))
+    	    p = psutil.Process(self.updatePID)
+    	    for child in p.children(recursive=True):
+        	    child.kill()
+    	    p.kill()
+    	    return False
+    	elif 'error:' in update_result:
+    	    error_list = []
+    	    with open('/home/pi/build_log.txt') as myFile:
+    		for num, line in enumerate(myFile, 1):
+    		    if 'error:' in line:
+    			    error_list.append(line)
+    	    compileError = '<pre>' + ''.join(error_list) + '</pre>'
+    	    self._logger.info("Update failed. Compiling error.")
+    	    self.isUpdating = False
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="failed", reason=compileError))
+    	    return False
+    	elif 'Make failed' in update_result:
+    	    self._logger.info("Update failed. Compiling error.")
+    	    self.isUpdating = False
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="failed", reason="Build failed."))
+    	    return False
+    	else:
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, status="continue"))
+    	    return True
+
+    def get_update_information(self):
+        return dict(
+            firmwareupdate_plugin=dict(
+                displayName="FirmwareUpdate Plugin",
+                displayVersion=self._plugin_version,
+                type="github_commit",
+                user="kevingelion",
+                repo="OctoPrint-FirmwareUpdate",
+                current=self._plugin_version,
+                pip="https://github.com/kevingelion/OctoPrint-FirmwareUpdate/archive/{target_version}.zip"
+            )
+        )
+
+    def get_api_commands(self):
+        return dict(
+            update_firmware=[],
+	        check_is_updating=[]
+        )
+
+    def on_api_command(self, command, data):
+    	if command == "update_firmware":
+    	    try:
+                os.remove('home/pi/build_log.txt')
+    	    except OSError:
+        		pass
+    	    f = open("/home/pi/build_log.txt", "w")
+            self._logger.info("Firmware update request has been made. Running...")
+            pro = Popen("cd /home/pi/git_repos/Marlin; ./build.sh", stdout=f, stderr=f, shell=True, preexec_fn=os.setsid)
+    	    self.isUpdating = True
+    	    self._logger.info("Setting isUpdating to " + str(self.isUpdating))
+    	    self.updatePID = pro.pid
+    	    self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, createPopup="yes"))
+    	    self.startTimer(1.0)
+    	
+    	elif command == "check_is_updating":
+    	    if self.isUpdating == True:
+    	        self._logger.info("Setting isUpdating to " + str(self.isUpdating))
+    	        self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, createPopup="yes"))
+    	    else:
+    	        self._logger.info("Setting isUpdating to " + str(self.isUpdating))
+    	        self._plugin_manager.send_plugin_message(self._identifier, dict(isupdating=self.isUpdating, deletePopup="yes"))
+    	else:
+    	    self._logger.info("Uknown command.")
+
+    def on_api_get(self, request):
+        return flask.make_response("Not found", 404)
+
+    def get_template_configs(self):
+        return [
+            dict(type="sidebar", name="Firmware Update", icon="refresh", data_bind="visible: loginState.isAdmin()"),
+	    ]
+
+__plugin_name__ = "Firmware Update Plugin"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = FirmwareUpdatePlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
---
+++ octoprint_firmwareupdate/static/js/firmwareupdate.js
@@ -0,0 +1,152 @@
+$(function() {
+  function FirmwareUpdateViewModel(parameters) {
+    var self = this;
+
+    self.printerState = parameters[0];
+    self.loginState = parameters[1];
+    self.connectionState = parameters[2];
+    self.settings = parameters[3];
+    self.popup = undefined;
+    self.isUpdating = ko.observable(undefined);
+    self.enableUpdating = ko.computed(function() {
+      return self.isUpdating() == false ? true : false;
+    });
+
+    self._showPopup = function(options, eventListeners) {
+      self._closePopup();
+      self.popup = new PNotify(options);
+
+      if (eventListeners) {
+        var popupObj = self.popup.get();
+        _.each(eventListeners, function(value, key) {
+          popupObj.on(key, value);
+        })
+      }
+    };
+
+    self._updatePopup = function(options) {
+      if (self.popup === undefined) {
+        self._showPopup(options);
+      } else {
+        self.popup.update(options);
+      }
+    };
+
+    self._closePopup = function() {
+      if (self.popup !== undefined) {
+        self.popup.remove();
+      }
+    };
+
+    self.onDataUpdaterReconnect = function() {
+      checkUpdating();
+    }
+
+    self.onUserLoggedIn = function(user) {
+      checkUpdating();
+    }
+
+    self.onDataUpdaterPluginMessage = function(plugin, data) {
+      if (plugin != "firmwareupdate") {
+        return;
+      }
+      if (data.hasOwnProperty("isupdating")) {
+        self.isUpdating(data.isupdating);
+        if (data.status == "failed") {
+          if (data.reason) {
+            $("#printer_connect").prop("disabled", false);
+            self._showPopup({
+              title: gettext("Update failed!"),
+              text: gettext("Updating your printer firmware was not successful.<br>" + pnotifyAdditionalInfo(data.reason)),
+              type: "error",
+              hide: false,
+              buttons: {
+                sticker: false
+              }
+            });
+          } else {
+            $("#printer_connect").prop("disabled", false);
+            self._showPopup({
+              title: gettext("Update failed!"),
+              text: gettext("Updating your printer firmware was not successful."),
+              type: "error",
+              hide: false,
+              buttons: {
+                sticker: false
+              }
+            });
+          }
+        } else if (data.status == "completed") {
+          $("#printer_connect").prop("disabled", false);
+          self._showPopup({
+            title: gettext("Update complete."),
+            text: gettext("The firmware on your printer has been successfully updated!"),
+            type: "success",
+            hide: false,
+            buttons: {
+              sticker: false
+            }
+          });
+        } else if (data.createPopup == "yes") {
+          $("#printer_connect").prop("disabled", true);
+          self._showPopup({
+            title: gettext("Updating..."),
+            text: gettext("Now updating, please wait."),
+            icon: "icon-cog icon-spin",
+            hide: false,
+            buttons: {
+              closer: false,
+              sticker: false
+            }
+          });
+        } else if (data.deletePopup == "yes") {
+          self._closePopup();
+        }
+      }
+    }
+
+    self.update_firmware_enable = function() {
+      self.isUpdating(false);
+    }
+
+    self.update_firmware = function() {
+      $("#printer_connect").prop("disabled", true);
+      $.ajax({
+        type: "POST",
+        url: "/api/plugin/firmwareupdate",
+        data: JSON.stringify({
+          command: 'update_firmware'
+        }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        complete: function(data) {
+          console.log(data);
+        },
+        error: function(jqXHR, exception) {
+          console.log(jqXHR);
+        }
+      });
+    };
+  }
+
+  function checkUpdating() {
+    console.log("Checking update status");
+    $.ajax({
+      type: "POST",
+      url: "/api/plugin/firmwareupdate",
+      data: JSON.stringify({
+        command: 'check_is_updating'
+      }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      error: function(jqXHR, exception) {
+        console.log(jqXHR);
+      }
+    });
+  }
+
+  ADDITIONAL_VIEWMODELS.push([
+    FirmwareUpdateViewModel, ["printerStateViewModel", "loginStateViewModel", "connectionViewModel", "settingsViewModel"],
+    ["#sidebar_plugin_firmwareupdate_wrapper"]
+  ]);
+});
